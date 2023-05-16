3f82cd49208fc912cda8e0848de2b91c54c32442
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2023-05-16 03:10:34 +0100

    - update kustomize manifest

--- modules/simple-producer/src/main/java/org/hung/kafka/SimpleProducerApplication.java
+++ modules/simple-producer/src/main/java/org/hung/kafka/SimpleProducerApplication.java
@@ -1,9 +1,13 @@
 package org.hung.kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -19,7 +23,17 @@ public class SimpleProducerApplication {
     @Autowired
     private CounterProducer counter;
 
-    @Scheduled(fixedRateString = "5000")
+    @Value("counter")
+    private String topic;
+
+    @Bean
+    public NewTopic counterTopic() {
+        return TopicBuilder.name(topic)
+                .partitions(10)
+                .build();
+    }
+
+    @Scheduled(fixedRateString = "10000")
     public void publishCounter() {
         counter.publish();
     }
